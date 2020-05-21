472b403f7d4be40435048a18e8debf2662e95a12
ferbr <ferbr@LAPTOP-PRRDH7J6>
2020-05-20 23:02:35 -0300

    Primeiro Commit - Projeto criado

---
+++ workshopmongo/src/test/java/com/nadisongasparato/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadisongasparato.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
