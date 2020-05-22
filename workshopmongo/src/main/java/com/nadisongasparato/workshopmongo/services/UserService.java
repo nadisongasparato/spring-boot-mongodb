0d2ecf9e834716e07cfee6a6378e9b638739ef3f
Nadison Gasparato <nadisongasparato@gmail.com>
2020-05-22 15:05:41 -0300

    Delecao de usuario com DELETE

--- workshopmongo/src/main/java/com/nadisongasparato/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/nadisongasparato/workshopmongo/services/UserService.java
@@ -31,6 +31,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findbyId(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
