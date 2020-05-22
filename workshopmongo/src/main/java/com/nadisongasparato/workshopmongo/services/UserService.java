e80ca79c7aa4b09e670f4c001e2b593969d344ff
Nadison Gasparato <nadisongasparato@gmail.com>
2020-05-22 14:43:28 -0300

    Obtendo usuario por id

--- workshopmongo/src/main/java/com/nadisongasparato/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/nadisongasparato/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.nadisongasparato.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nadisongasparato.workshopmongo.domain.User;
 import com.nadisongasparato.workshopmongo.repository.UserRepository;
+import com.nadisongasparato.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findbyId(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 }
