d971bd7875bd3b5e71418187a2e0940ca3b25b55
Nadison Gasparato <nadisongasparato@gmail.com>
2020-05-25 20:59:34 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/com/nadisongasparato/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.nadisongasparato.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nadisongasparato.workshopmongo.domain.Post;
+import com.nadisongasparato.workshopmongo.repository.PostRepository;
+import com.nadisongasparato.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findbyId(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
